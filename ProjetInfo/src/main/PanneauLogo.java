7956c209a0bf406cc4754b8d2eb57d1562133bf9
awahbi <awahbi@d952dbb2-b344-485c-b755-fa21aebe243d>
2013-05-22 09:56:49 +0000

    Modification panneau principal

---
+++ ProjetInfo/src/main/PanneauLogo.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class PanneauLogo extends JPanel {
+	private Image imgLogo; 
+	
+	public PanneauLogo(){
+		readImages();
+		
+	}
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+
+		int positionX = this.getWidth();
+		int positionY = this.getHeight();
+		g.drawImage(imgLogo, 20, 20, positionX-200, positionY-100, null);
+	}
+	
+	
+	/**
+	 * Met les images en memoire
+	 */
+	private void readImages(){
+		try {
+			imgLogo = ImageIO.read(new File("./images/Logo.png"));
+
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
--- ProjetInfo/src/main/PanneauPrincipal.java
+++ ProjetInfo/src/main/PanneauPrincipal.java
@@ -4,11 +4,14 @@ import java.awt.*;
 import javax.swing.*;
 
 public class PanneauPrincipal extends JFrame {
-	private JPanel panel = new JPanel();
+	private PanneauLogo panel = new PanneauLogo();
+	private JPanel panBienvenue = new JPanel();
 	private JPanel panneauboutons = new JPanel();
 	private JButton bouton1 =new JButton("Mode Initiation");
 	private JButton bouton2 =new JButton("Mode Jeu");
 	private JButton bouton3 =new JButton("Choix niveau");
+	private JLabel bienvenue=new JLabel("Bienvenue au jeu ! Amusez vous bien =]");
+	
 	
 	public PanneauPrincipal(){
 		setTitle("HandizGame");
@@ -22,10 +25,17 @@ public class PanneauPrincipal extends JFrame {
 	private void jbInit() {
 		
 		this.setContentPane(panel);
-		this.setBackground(Color.lightGray);
+		panel.setBackground(Color.WHITE);
 		panel.setLayout(new BorderLayout());
 		panel.add(panneauboutons, BorderLayout.EAST);
+		panel.add(panBienvenue, BorderLayout.NORTH);
+		panBienvenue.setPreferredSize(new Dimension(50,60));
+		panBienvenue.setBackground(Color.white);
+		panBienvenue.setLayout(new BorderLayout());
+		panBienvenue.add(bienvenue, BorderLayout.CENTER);
 		
+		
+		panneauboutons.setBackground(Color.white);
 	
 		panneauboutons.setPreferredSize(new Dimension(150,600));
 		panneauboutons.setLayout(new FlowLayout());
@@ -34,9 +44,7 @@ public class PanneauPrincipal extends JFrame {
 		bouton3.setPreferredSize(new Dimension(140,60));
 		panneauboutons.add(bouton1);
 		panneauboutons.add(bouton2);
-		panneauboutons.add(bouton3);
-		
-				
+		panneauboutons.add(bouton3);		
 		
 	}
 
