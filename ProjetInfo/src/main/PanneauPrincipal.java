fd56ae9d83be114dab0a1593cd58c6444c4dcc3b
plaurenson <plaurenson@d952dbb2-b344-485c-b755-fa21aebe243d>
2013-06-02 16:24:31 +0000

    Maj6

--- ProjetInfo/src/main/PanneauPrincipal.java
+++ ProjetInfo/src/main/PanneauPrincipal.java
@@ -60,9 +60,15 @@ public class PanneauPrincipal extends JPanel {
 		this.add(panel);
 
 		panel.setBackground(Ressources.GRIS);
+<<<<<<< .mine
+		panneauboutons.setOpaque(false);
+		
+		
+=======
 		panBienvenue.setBackground(Color.gray);
 		panel.setBackground(Ressources.GRIS);
 
+>>>>>>> .r79
 		panel.setLayout(new BorderLayout());
 		panel.add(panneauboutons, BorderLayout.EAST);
 		panel.add(panBienvenue, BorderLayout.NORTH);
